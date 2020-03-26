48a1f9ffa1649ffca32be19722aebe2316032936
reza-VirtualBox <rasayesh@email.arizona.edu>
2020-03-26 04:35:38 -0700

    adding helloworld files

---
+++ code/helloworld.asm
@@ -0,0 +1,17 @@
+section .data
+	Hello db "Hello World!",10,0
+	len equ $ - Hello
+
+section .text
+	global _start
+
+_start:
+	mov eax,4
+	mov ebx,1
+	mov ecx,Hello
+	mov edx,len
+	int 80h
+
+	mov eax,1
+	mov ebx,0
+	int 0x80
