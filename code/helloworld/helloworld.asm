19e804db5782d8b25bf8e859a2c29db7ba7173b4
reza-VirtualBox <rasayesh@email.arizona.edu>
2020-03-26 19:19:57 -0700

    added comments to helloworld code

--- code/helloworld/helloworld.asm
+++ code/helloworld/helloworld.asm
@@ -1,17 +1,17 @@
 section .data
-	Hello db "hello world",10,0
-	len equ $ - Hello
+	Hello db "hello world",10,0	;Hello = "hello world\n"
+	len equ $ - Hello		;get length of Hello
 
 section .text
 	global _start
 
 _start:
-	mov eax,4	
-	mov ebx,1	
-	mov ecx,Hello	
-	mov edx,len	
-	int 80h
+	mov eax,4	;eax = 4
+	mov ebx,1	;ebx = 1
+	mov ecx,Hello	;ecx = Hello
+	mov edx,len	;edx = len
+	int 80h		;interrupt code 80h
 
-	mov eax,1 
-	mov ebx,0
-	int 0x80
+	mov eax,1 	;system exit call 
+	mov ebx,0	;argument returned 0
+	int 0x80	;interrupt code 0x80
