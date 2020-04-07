f1a2050f03caf02879613978eb2fb313d6984903
martin315 <martin315@DESKTOP-6L09GA2.localdomain>
2020-04-06 21:29:30 -0700

    Jump code snip added

---
+++ code/MileStone2/jump.asm
@@ -0,0 +1,23 @@
+section .data
+	Msg1 db "This won't be printed.",10,0	;Msg1 = "is  won't be  printed.\n"
+	Msg1Len equ $ - Msg1		            ;get length of Hello
+    Msg2 db "This will be printed.",10,0	;Hello = "hello world\n"
+	Msg2Len equ $ - Msg2		            ;get length of Hello
+
+section .text
+	global _start
+
+_start:
+	mov eax,4	    ;eax = 4
+	mov ebx,1	    ;ebx = 1
+	mov ecx,Msg2	;ecx = Msg2
+	mov edx,Msg2Len	;edx = Msg2Len
+    jmp skip        ;jumps to skip
+	mov ecx,Msg2	;ecx = Msg2
+	mov edx,Msg2Len	;edx = Msg2Len
+skip:
+	int 80h		    ;interrupt code 80h
+
+	mov eax,1 	;system exit call 
+	mov ebx,0	;argument returned 0
+	int 0x80	;interrupt code 0x80
