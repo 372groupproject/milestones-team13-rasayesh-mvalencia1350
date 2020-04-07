f53b026ddb664054c50c8c5805eca3baf52d2dd2
reza-VirtualBox <rasayesh@email.arizona.edu>
2020-04-06 22:12:47 -0700

    its lit 2

--- code/MileStone2/jumpLessThan/jumpLessThan.asm
+++ code/MileStone2/jumpLessThan/jumpLessThan.asm
@@ -1,25 +1,25 @@
 section .data
-	equal db "eax is equal to 1",10,0	;equal = "eax is equal to 1\n"
-	len equ $ - equal			;get length of equal
+	less db "eax is less than 10",10,0	;equal = "eax is less than 10 \n"
+	len equ $ - less			;get length of equal
 
 global _start
 
 section .text
 _start:
 	mov ebx, 0	; set ebx to 0
-	mov eax, 1	; set eax to 1
-	cmp eax, 1	; compare eax to 1
-	je equalTo1	; jump if eax is equal to 1
+	mov eax, 5	; set eax to 5
+	cmp eax, 10	; compare eax to 10
+	jl lessThan10	; jump if eax is less than 10
 	mov ebx, 100	; set exit status to 100
 	int 0x80	; interrupt code = 80
-	jmp end		; if eax not equal to 1 end program.
+	jmp end		; if eax is not less than 10 end program.
 
-equalTo1:
+lessThan10:
 	mov eax,4	;eax = 4
 	mov ebx,1	;ebx = 1
-	mov ecx,equal	;ecx = equal
+	mov ecx,less	;ecx = less
 	mov edx,len	;edx = len
-	int 80h		;interrupt code 80h
+	int 0x80	;interrupt code 0x80
 
 	mov eax,1 	;system exit call 
 	mov ebx,0	;argument returned 0
@@ -29,3 +29,4 @@ end:
 	mov eax,1 	;system exit call 
 	mov ebx,0	;argument returned 0
 	int 0x80	;interrupt code 0x80
+
